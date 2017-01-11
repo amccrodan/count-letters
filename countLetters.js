a1e3e9ae5466b130ffef13bab7c078fc3f4b92bb
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-11 01:38:10 +0000

    completed basic functionality

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,21 @@
 function countLetters(inputString) {
+  stringObj = {};
 
+  for (var i = 0; i < inputString.length; i++) {
+    var char = inputString.charAt(i);
+
+    if (char === " ") {
+      continue;
+    }
+
+    if (stringObj[char]) {
+      stringObj[char] += 1;
+    } else {
+      stringObj[char] = 1;
+    }
+  }
+
+  return stringObj;
 }
 
-countLetters("This is a test");
+console.log(countLetters("This is a test"));
